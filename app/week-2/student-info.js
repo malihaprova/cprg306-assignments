f353ade726383588e6bc9dc3d3e2fd96701617e7
malihaprova <malihaprova@gmail.com>
2024-09-24 10:13:17 -0600

    Week 2 Submission

---
+++ app/week-2/student-info.js
@@ -0,0 +1,8 @@
+import Link from 'next/link'
+    export default function StudentInfo() {
+        return (
+        <div><p>Maliha Prova</p>
+        <Link href="https://github.com/malihaprova/cprg306-assignments">Github Repo</Link>
+        </div>
+        );
+      }

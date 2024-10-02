d742c4c3ad0dc4c195b6cf2f8ac15c8cd39e8f2e
malihaprova <malihaprova@gmail.com>
2024-10-01 23:42:26 -0600

    week 3

--- app/week-3/item.js
+++ app/week-3/item.js
@@ -1,8 +1,8 @@
 export default function Item({name, quantity, category}) {
-    return (<li>
-                <h2>{name}</h2>
-                <div>
-                    <p>Buy {quantity} in {category}</p>
+    return (<li class="p-2 m-2 bg-zinc-800 max-w-sm">
+                <h2 class="text-xl font-bold">{name}</h2>
+                <div class="text-sm">
+                    Buy {quantity} in {category}
                 </div>
             </li>);
 }
--- app/week-3/page.js
+++ app/week-3/page.js
@@ -2,7 +2,7 @@ import ItemList from './item-list.js';
 export default function Page() {
     return (
       <main>
-        <h1>Shopping List</h1>
+        <h1 class="text-3xl font-bold m-2">Shopping List</h1>
         <ItemList />
       </main>
     );

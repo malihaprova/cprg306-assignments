6423e2d66809629407e851ea89da57974b3b47b5
malihaprova <malihaprova@gmail.com>
2024-10-09 00:00:59 -0600

    Update page.js

--- app/week-4/page.js
+++ app/week-4/page.js
@@ -18,9 +18,9 @@ export default function NewItem() {
                 <div class="flex justify-between">
                     <span class="m-1 text-black">{quantity}</span>
                     <div class="flex">
-                        <button class="m-1 w-8 bg-blue-500 text-white font-semibold rounded-lg shadow-md hover:bg-blue-700 focus:outline-none focus:ring-2 disabled:bg-gray-400 focus:ring-blue-400 focus:ring-opacity-75" 
+                        <button class="m-1 w-8 bg-orange-500 text-white font-semibold rounded-lg shadow-md hover:bg-orange-700 focus:outline-none focus:ring-2 disabled:bg-gray-400 focus:ring-orange-400 focus:ring-opacity-75" 
                         onClick={decrement}>-</button>
-                        <button class="m-1 w-8 bg-blue-500 text-white font-semibold rounded-lg shadow-md hover:bg-blue-700 focus:outline-none focus:ring-2 disabled:bg-gray-400 focus:ring-blue-400 focus:ring-opacity-75" 
+                        <button class="m-1 w-8 bg-orange-500 text-white font-semibold rounded-lg shadow-md hover:bg-orange-700 focus:outline-none focus:ring-2 disabled:bg-gray-400 focus:ring-orange-400 focus:ring-opacity-75" 
                         onClick={increment}>+</button>
                     </div>
                 </div>
